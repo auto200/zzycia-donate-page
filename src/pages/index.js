3bb757302e54238ef8e44791c242563331210e04
auto200 <michal.warac@gmail.com>
2021-02-15 13:54:04 +0100

    Extract constants, handle message and url forming

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,49 +15,29 @@ import {
   Center,
   Heading,
   InputRightAddon,
+  Box,
+  chakra,
+  useClipboard,
 } from "@chakra-ui/react";
-
-const HOTPAY_SECRET =
-  "UVhwS0FmMVQ4cy9KaWlaNjZkZGp0SndvRnRGUmRHWTYrTzEzalJHSHNkWT0%2C";
+import {
+  defaultOptions,
+  ENCODED_MESSAGE_MAX_LENGTH,
+  HOTPAY_SECRET,
+} from "../utils/constants";
 
 const IndexPage = () => {
-  const [options, setOptions] = useState({
-    1: {
-      ammout: "10",
-      currency: "zł",
-      texts: ["na bułki", "na rybe"],
-    },
-    2: {
-      ammout: "20",
-      currency: "zł",
-      texts: ["cel na 20 zl", "kolejny cel 20 zl", "cos za 2 dyszki"],
-    },
-    3: {
-      ammout: "50",
-      currency: "zł",
-      texts: ["cel na 50 zl", "kolejny cel 50 zl", "cos za 5 dyszek"],
-    },
-    4: {
-      ammout: "100",
-      currency: "zł",
-      texts: ["cel na 100 zl", "kolejny cel 100 zl", "cos za 1OO zł"],
-    },
-    custom: {
-      ammout: "",
-      currency: "zł",
-      texts: ["tekst przy dowolnej kwocie", "dowolna kwota", "custom"],
-    },
-  });
-  const [randomTexts] = useState(
+  const [options, setOptions] = useState(defaultOptions);
+  const [randomTexts] = useState(() =>
     Object.values(options).map(({ texts }) => sample(texts))
   );
 
-  const [message, setMessage] = useState("");
+  const [encodedMessage, setEncodedMessage] = useState("");
   const [activeOptionId, setActiveOptionId] = useState("");
   const [canSend, setCanSend] = useState(false);
   const [hotpayUrl, setHotpayUrl] = useState("");
+  const { onCopy } = useClipboard(hotpayUrl);
 
-  const customAmmountInputRef = useRef();
+  const customAmmountInputRef = useRef(null);
 
   useEffect(() => {
     if (!activeOptionId) {
@@ -75,6 +55,17 @@ const IndexPage = () => {
     setCanSend(false);
   }, [activeOptionId, options.custom.ammout]);
 
+  useEffect(() => {
+    if (!activeOptionId) return;
+    const url = new URL("https://platnosc.hotpay.pl");
+    url.searchParams.append("SEKRET", HOTPAY_SECRET);
+    url.searchParams.append("KWOTA", options[activeOptionId].ammout);
+    url.searchParams.append("NAZWA_USLUGI", "tutaj bedzie nazwa uslugi");
+    url.searchParams.append("ADRES_WWW", "https://thank-you-url.pl");
+    url.searchParams.append("ID_ZAMOWIENIA", encodedMessage);
+    setHotpayUrl(url.href);
+  }, [options.custom.ammout, activeOptionId]);
+
   return (
     <>
       <SEO title="Home" />
@@ -84,95 +75,95 @@ const IndexPage = () => {
         </Heading>
       </Center>
       <Center m="2">
-        <Stack w="100%" maxW="550px">
-          {Object.entries(options).map(([id, { ammout, currency }], i) => {
-            return (
-              <React.Fragment key={id}>
-                <HStack
-                  onClick={() => {
-                    setActiveOptionId(id);
-                    if (id === "custom") customAmmountInputRef.current.focus();
-                  }}
-                  _hover={{
-                    cursor: "pointer",
-                    bgColor: "gray.700",
-                    "& button, & div div": { bgColor: "green.500" },
-                  }}
-                  bgColor={activeOptionId === id && "gray.600"}
-                  rounded="lg"
-                  p={2}
-                >
-                  {id === "custom" ? (
-                    <InputGroup w="28">
-                      <Input
-                        ref={customAmmountInputRef}
-                        type="number"
-                        placeholder="..."
-                        value={options.custom.ammout}
-                        onChange={e =>
-                          setOptions({
-                            ...options,
-                            custom: {
-                              ...options.custom,
-                              ammout: e.target.value,
-                            },
-                          })
-                        }
-                      />
-                      <InputRightAddon
-                        bgColor={activeOptionId === id && "green.600"}
-                      >
+        <chakra.form
+          w="100%"
+          maxW="550px"
+          onSubmit={e => {
+            e.preventDefault();
+            window.open(hotpayUrl, "_self");
+          }}
+        >
+          <Stack>
+            {Object.entries(options).map(([id, { ammout, currency }], i) => {
+              const active = activeOptionId === id;
+              return (
+                <React.Fragment key={id}>
+                  <HStack
+                    onClick={() => {
+                      setActiveOptionId(id);
+                      if (id === "custom")
+                        customAmmountInputRef.current.focus();
+                    }}
+                    _hover={{
+                      cursor: "pointer",
+                      bgColor: "gray.700",
+                      "& button, & div div": { bgColor: "green.500" },
+                    }}
+                    bgColor={active && "gray.600"}
+                    rounded="lg"
+                    p={2}
+                  >
+                    {id === "custom" ? (
+                      <InputGroup w="28">
+                        <Input
+                          ref={customAmmountInputRef}
+                          type="number"
+                          placeholder="..."
+                          value={options.custom.ammout}
+                          onChange={e =>
+                            setOptions({
+                              ...options,
+                              custom: {
+                                ...options.custom,
+                                ammout: e.target.value,
+                              },
+                            })
+                          }
+                        />
+                        <InputRightAddon bgColor={active && "green.600"}>
+                          {currency}
+                        </InputRightAddon>
+                      </InputGroup>
+                    ) : (
+                      <Button m="2" bgColor={active ? "green.600" : "gray.700"}>
+                        {ammout}
                         {currency}
-                      </InputRightAddon>
-                    </InputGroup>
-                  ) : (
-                    <Button
-                      m="2"
-                      bgColor={activeOptionId === id ? "green.600" : "gray.700"}
-                    >
-                      {ammout}
-                      {currency}
-                    </Button>
-                  )}
-                  <Text>{randomTexts[i]}</Text>
-                </HStack>
-                <Divider />
-              </React.Fragment>
-            );
-          })}
-          <Textarea
-            disabled={!canSend}
-            maxLength={250}
-            placeholder="Twoja wiadomość"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-          />
-          <Button
-            disabled={!canSend}
-            as="a"
-            //remember to use encodeuricomponent!!
-            // href={
-            //   canSend
-            //  hotpayUrl
-            //     : null
-            // }
-            onClick={() => {
-              const url = new URL("https://platnosc.hotpay.pl");
-              url.searchParams.append("SEKRET", HOTPAY_SECRET);
-              url.searchParams.append("KWOTA", options[activeOptionId].ammout);
-              url.searchParams.append(
-                "NAZWA_USLUGI",
-                "tutaj bedzie nazwa uslugi"
+                      </Button>
+                    )}
+                    <Text>{randomTexts[i]}</Text>
+                  </HStack>
+                  <Divider />
+                </React.Fragment>
               );
-              url.searchParams.append("ADRES_WWW", "https://thank-you-url.pl");
-              url.searchParams.append("ID_ZAMOWIENIA", message);
-              setHotpayUrl(url.href);
-            }}
-          >
-            Dalej
-          </Button>
-          <Text>{hotpayUrl}</Text>
-        </Stack>
+            })}
+            <Box pos="relative">
+              <Textarea
+                disabled={!canSend}
+                placeholder="Twoja wiadomość"
+                value={decodeURIComponent(encodedMessage)}
+                onChange={e => {
+                  const message = encodeURIComponent(e.target.value);
+                  if (message.length <= ENCODED_MESSAGE_MAX_LENGTH) {
+                    setEncodedMessage(message);
+                  }
+                }}
+              />
+              <Text pos="absolute" bottom="2" right="2">
+                {encodedMessage.length}/{ENCODED_MESSAGE_MAX_LENGTH}
+              </Text>
+            </Box>
+            <Button
+              disabled={!canSend}
+              type="submit"
+              bgColor={canSend && "green.500"}
+            >
+              Dalej
+            </Button>
+            <Text onClick={onCopy} _hover={{ cursor: "pointer" }}>
+              {hotpayUrl}
+            </Text>
+          </Stack>
+        </chakra.form>
       </Center>
     </>
   );
---
+++ src/utils/constants.js
@@ -0,0 +1,32 @@
+export const defaultOptions = {
+  1: {
+    ammout: "10",
+    currency: "zł",
+    texts: ["na bułki", "na rybe"],
+  },
+  2: {
+    ammout: "20",
+    currency: "zł",
+    texts: ["cel na 20 zl", "kolejny cel 20 zl", "cos za 2 dyszki"],
+  },
+  3: {
+    ammout: "50",
+    currency: "zł",
+    texts: ["cel na 50 zl", "kolejny cel 50 zl", "cos za 5 dyszek"],
+  },
+  4: {
+    ammout: "100",
+    currency: "zł",
+    texts: ["cel na 100 zl", "kolejny cel 100 zl", "cos za 1OO zł"],
+  },
+  custom: {
+    ammout: "",
+    currency: "zł",
+    texts: ["tekst przy dowolnej kwocie", "dowolna kwota", "custom"],
+  },
+};
+
+export const HOTPAY_SECRET =
+  "UVhwS0FmMVQ4cy9KaWlaNjZkZGp0SndvRnRGUmRHWTYrTzEzalJHSHNkWT0%2C";
+
+export const ENCODED_MESSAGE_MAX_LENGTH = 64;
