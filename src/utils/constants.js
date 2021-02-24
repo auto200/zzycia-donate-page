0f333c45fab2c5270e235b882e6b63f36af82131
auto200 <michal.warac@gmail.com>
2021-02-24 10:39:45 +0100

    Update texts

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,27 +2,51 @@ export const defaultOptions = {
   1: {
     ammout: "10",
     currency: "zł",
-    texts: ["na bułki", "na rybe"],
+    texts: [
+      "na śniadanie",
+      "na kwiaty dla lubej",
+      "na piwo",
+      "na śledzika",
+      "na cebule",
+      "na kupon Lotto",
+      "na łakocie",
+      "na kawę",
+    ],
   },
   2: {
     ammout: "20",
     currency: "zł",
-    texts: ["cel na 20 zl", "kolejny cel 20 zl", "cos za 2 dyszki"],
+    texts: [
+      "kebab i piwko",
+      "na flaszkę",
+      "na Fastfood",
+      "na bukiet dla Lubej",
+      "na prawdziwe męskie kosmetyki - pumeks",
+    ],
   },
   3: {
     ammout: "50",
     currency: "zł",
-    texts: ["cel na 50 zl", "kolejny cel 50 zl", "cos za 5 dyszek"],
+    texts: ["na szampon do brody", "na olejek do brody", "na pizze", "na wino"],
   },
   4: {
     ammout: "100",
     currency: "zł",
-    texts: ["cel na 100 zl", "kolejny cel 100 zl", "cos za 1OO zł"],
+    texts: [
+      "na kolację z Lubą",
+      "na rachunki",
+      "na barbera",
+      "na kredyt",
+      "na wakacje",
+      "na kino",
+      "na fryzjera",
+      "na wyjazd gdzieś tam",
+    ],
   },
   custom: {
     ammout: "",
     currency: "zł",
-    texts: ["tekst przy dowolnej kwocie", "dowolna kwota", "custom"],
+    texts: ["zaskocz mnie!", "oj, zaszaleje!", "deszcz pieniędzy!"],
   },
 };
 
