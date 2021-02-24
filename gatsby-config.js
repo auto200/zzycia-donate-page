e2dda8327b8af8c0026884e55cb8e933f6af8c39
auto200 <michal.warac@gmail.com>
2021-02-24 10:40:50 +0100

    Update metadata, title, icon, desc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wesprzyj Sebka 🧔`,
+    description: `Tutaj możesz wesprzeć Sebka, autora kanału z życia na youtube`,
+    author: `Sebek`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -22,17 +22,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wesprzyj Sebka 🧔`,
+        short_name: `🧔`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1A202C`,
+        theme_color: `#1A202C`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
